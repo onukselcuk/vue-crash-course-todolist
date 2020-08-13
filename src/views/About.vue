301aa41796fbef4a57fb7a651b338d6b2ff746d6
Selcuk <devonukselcuk@gmail.com>
2020-08-13 10:50:12 +0300

    complete project

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <p>
+            This is the TodoList app v1.0.0. It is part of the Vue crash course.
+        </p>
+    </div>
+</template>
